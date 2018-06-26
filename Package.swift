16f40d84ade595672f32a6184eb933c867690ffe
Nick Dawson <nick@frollo.us>
2018-06-26 10:37:47 +1000

    [#158316919] Restructure and add dependencies

---
+++ Package.swift
@@ -0,0 +1,5 @@
+
+
+dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.7.2")
+]
---
+++ Sources/FrolloSDK.h
@@ -0,0 +1,18 @@
+//
+//  FrolloSDK.h
+//  FrolloSDK
+//
+//  Created by Nick Dawson on 26/6/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrolloSDK.
+FOUNDATION_EXPORT double FrolloSDKVersionNumber;
+
+//! Project version string for FrolloSDK.
+FOUNDATION_EXPORT const unsigned char FrolloSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrolloSDK/PublicHeader.h>
+
+
