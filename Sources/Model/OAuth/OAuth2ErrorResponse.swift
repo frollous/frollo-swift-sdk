d45967bd7dc485558cb74ad921674ac1f9decfdf
Dipesh Dhakal <dipesh@frollo.us>
2019-03-26 13:36:26 +1100

    Documentation

--- Sources/Model/OAuth/OAuth2ErrorResponse.swift
+++ Sources/Model/OAuth/OAuth2ErrorResponse.swift
@@ -18,25 +18,48 @@ import Foundation
 
 public struct OAuth2ErrorResponse: Codable {
     
+    /// Type of Error
     public enum ErrorType: String, Codable, CaseIterable {
+        
+        /// The request is missing a parameter so the server can’t proceed with the request.
         case invalidRequest = "invalid_request"
+        
+        /// Client authentication failed, such as if the request contains an invalid client ID or secret.
         case invalidClient = "invalid_client"
+        
+        /// The authorization code (or user’s password for the password grant type) is invalid or expired.
         case invalidGrant = "invalid_grant"
+        
+        /// For access token requests that include a scope (password or client_credentials grants), this error indicates an invalid scope value in the request.
         case invalidScope = "invalid_scope"
+        
+        /// This client is not authorized to use the requested grant type.
         case unauthorizedClient = "unauthorized_client"
+        
+        /// If a grant type is requested that the authorization server doesn’t recognize, use this code.
         case unsupportedGrantType = "unsupported_grant_type"
+        
+        /// Server error
         case serverError = "server_error"
+        
+        /// Any other errors, default value
         case oAuth2Error = "oauth2_error"
     }
     
+    /// Coding keys
     public enum CodingKeys: String, CodingKey {
         case errorDescription = "error_description"
         case errorUri = "error_uri"
         case errorType = "error"
     }
     
+    /// Type of error
     public let errorType: ErrorType
+    
+    /// Description or error
     public let errorDescription: String?
+    
+    /// Optional Uri from Authentication Server
     public let errorUri: String?
     
 }
