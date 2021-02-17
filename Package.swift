2c9f821f626a8ef06557e117132259d14b2fa116
Maher Santina <maher.santina90@gmail.com>
2021-02-17 11:25:54 +1100

    WIP

--- Package.swift
+++ Package.swift
@@ -1,26 +1,39 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FrolloSDK",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FrolloSDK",
             targets: ["FrolloSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0"))
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
+        .package(name: "AppAuth", url: "https://github.com/openid/AppAuth-iOS.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMajor(from: "5.0.0")),
+
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FrolloSDK",
-            dependencies: [],
-            path: "./Sources/"),
-        .testTarget(
-            name: "FrolloSDKTests",
-            dependencies: ["FrolloSDK"],
-            path: "./Tests/"),
+            dependencies: ["Alamofire", "AppAuth", "SwiftyJSON"],
+            resources: [.process("Resources")]
+        ),
+//        .testTarget(
+//            name: "FrolloSDKTests",
+//            dependencies: ["FrolloSDK", "OHHTTPStubs", .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs")],
+//            resources: [.process("Resources")]
+//        ),
     ]
 )
